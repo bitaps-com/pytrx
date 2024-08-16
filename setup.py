9fd131a7a19054a555459a0158ec707f639fc61c
nadyka <nadyka@bitaps.com>
2024-08-17 01:05:12 +0400

    add requirements

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(name='pytrx',
       author_email='nadyka@bitaps.com',
       license='GPL-3.0',
       packages=find_packages(),
-      install_requires=['py-ecc==1.4.2','rlp==0.6.0','coincurve','pysha3==1.0.2', 'requests==2.32.3'],
+      install_requires=['py-ecc==1.4.2','rlp==0.6.0','coincurve','pysha3==1.0.2', 'requests==2.32.3', 'base58==2.1.1'],
       include_package_data=True,
       package_data={
           'pytrx': ['bip39_word_list/*.txt'],
